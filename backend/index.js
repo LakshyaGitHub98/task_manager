5d09b34b8ca95cc64bbcdad9d4e3bdcd48b8bc1a
LakshyaGitHub98 <laktripathi9839@gmail.com>
2025-07-04 08:34:44 +0530

    Adding cors library

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ const app = require('./routes/app');
 const port = process.env.PORT  || 3000;
 //const connectDB = require('./src/db');
 const loginRoutes = require('./routes/login');
-
+const cors = require('cors');
 //connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
