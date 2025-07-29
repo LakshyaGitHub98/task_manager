3df7d166357f817d8f1925c4bd242de30b3ef158
LakshyaGitHub98 <laktripathi9839@gmail.com>
2025-07-29 08:21:06 +0530

    Connection to Database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = require('./routes/app');
 const port = process.env.PORT  || 3000;
-//const connectDB = require('./src/db');
+const connectDB = require('./src/db');
 const loginRoutes = require('./routes/login');
 const cors = require('cors');
-//connectDB();
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,20 +15,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-
-  // Dummy check — replace with real DB logic
-  if (username === 'admin' && password === '123456') {
-    return res.status(200).json({ message: 'Login successful', token: 'abc123' });
-  } else {
-    return res.status(401).json({ message: 'Invalid credentials' });
-  }
-});
-
-
-
-//app.post('/api/login',loginRoutes);
+app.post('/api/login',loginRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- backend/src/db.js
+++ backend/src/db.js
@@ -4,10 +4,7 @@ const mongoDb_uri = process.env.MONGO_URI || 'mongodb://localhost:27017/task-man
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoDb_uri);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error);
